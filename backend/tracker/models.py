d406a1d8a721eda62c109154968ec0c37dfcda52
Michał <michal.olszewski@vip.onet.pl>
2026-03-02 17:05:36 +0100

    Define JobApplication model and register tracker app

--- backend/tracker/models.py
+++ backend/tracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class JobApplication(models.Model):
+    STATUS_CHOICES = [
+        ("applied", "Wysłano"),
+        ("interview", "Interview"),
+        ("rejected", "Rejected"),
+        ("offer", "Offer"),
+    ]
+
+    company_name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    offer_utl = models.URLField(blank=True)
+    applied_date = models.DateField(auto_now_add=True)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="applied")
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.position} w {self.company_name}"
